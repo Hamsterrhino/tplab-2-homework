dc82984208bcf47a420bee58e2cf3533ee8b094d
Hamsterrhino <vovka20011@gmail.com>
2020-06-01 09:26:10 +0000

    Add files via upload

---
+++ hello_world.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+/**
+ * main
+ * @brief reads username from standart input stream, writes greetings message
+ * to standart output stream
+ * @returns 0 if no error occures
+ */
+<<<<<<< HEAD
+int main(){
+    std::string name;
+    std::cin>>name; 
+    // Read name from standart input stream
+    std::cout<<"Hello world from "<<name<<std::endl; 
+    // Write greeting string
+    return 0;
+=======
+int
+main()
+{
+  std::string name;
+  std::cin >> name; // Read name from standart input stream
+  std::cout << "Hello world from " << name
+            << std::endl; // Write greeting string
+  return 0;
+>>>>>>> add clang-format and format files using it
+}
+
+
